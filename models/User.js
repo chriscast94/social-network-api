b1ceccb20638605ed64dc47886a4561f4e76f2f0
Christian Castillo <chriscast94@outlook.com>
2021-12-18 12:14:54 -0800

    initial commit

---
+++ models/User.js
@@ -0,0 +1,55 @@
+const { Schema, model } = require('mongoose');
+
+
+const userSchema = new Schema({
+    userName: {
+        type: String,
+        required: true,
+        max_length: 50,
+    },
+    last: {
+        type: String,
+        required: true,
+        max_length: 50,
+    },
+    github: {
+        type: String,
+        required: true,
+        max_length: 50,
+    },
+    assignments: [assignmentSchema],
+})
+
+
+username
+
+String
+Unique
+Required
+Trimmed
+
+
+
+email
+
+String
+Required
+Unique
+Must match a valid email address(look into Mongoose's matching validation)
+
+
+
+thoughts
+
+Array of _id values referencing the Thought model
+
+
+
+friends
+
+Array of _id values referencing the User model(self - reference)
+
+
+
+Schema Settings:
+    Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
